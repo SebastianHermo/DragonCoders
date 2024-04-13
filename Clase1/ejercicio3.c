a4763cf3f0b4fc342ab4d7024d4d8dfc1f936129
LucasR1006 <lucasor1006@gmail.com>
2024-04-13 12:19:56 -0300

    subo ej 3

---
+++ Clase1/ejercicio3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int lista[] = {1, 25, 36, 46, 20, 1};
+    int minLista = lista[0];
+   
+    for (int i = 1; i < sizeof(lista) / sizeof(int); i++){
+      if (lista[i] < minLista) {
+        minLista = lista[i];
+      } 
+    } 
+
+   printf("Minimo: %d", minLista);
+   
+    return 0;
+}
