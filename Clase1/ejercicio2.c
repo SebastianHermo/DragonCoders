bb627aea415b8d4f029bcf055e4c1c2fe0cb5e60
LucasR1006 <lucasor1006@gmail.com>
2024-04-13 12:14:34 -0300

    subo ej 2

---
+++ Clase1/ejercicio2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int lista[] = {1, 25, 36, 46, 20, 0};
+    int maxLista = lista[0];
+   
+    for (int i = 1; i < sizeof(lista) / sizeof(int); i++){
+      if (lista[i] > maxLista) {
+        maxLista = lista[i];
+      } 
+    } 
+
+   printf("Maximo: %d", maxLista);
+   
+    return 0;
+}
