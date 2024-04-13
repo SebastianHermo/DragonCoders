77e9e081157a8dd7ad055bba60a3e9840390b7f2
LucasR1006 <lucasor1006@gmail.com>
2024-04-13 12:26:02 -0300

    subo ej 3.1

--- Clase1/ejercicio3.c
+++ Clase1/ejercicio3.c
@@ -5,7 +5,7 @@ int main(){
     int lista[] = {1, 25, 36, 46, 20, 1};
     int minLista = lista[0];
    
-    for (int i = 1; i < sizeof(lista) / sizeof(int); i++){
+    for (int i = 0; i < sizeof(lista) / sizeof(int); i++){
       if (lista[i] < minLista) {
         minLista = lista[i];
       } 
---
+++ Clase1/ejercicio4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int lista[] = {1, 5 ,6};
+    double promedio = lista[0];
+
+    for int i = 0
+    
+    
+    
+    
+    return 0;
+}
