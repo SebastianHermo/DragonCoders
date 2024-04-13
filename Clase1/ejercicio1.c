7ec7480a03ee5b893bb8562491bd1ef5be95110f
LucasR1006 <lucasor1006@gmail.com>
2024-04-13 13:16:01 -0300

    subo ej 4 sin terminar

--- Clase1/ejercicio1.c
+++ Clase1/ejercicio1.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 
 int main(){
     char nombre [100];
@@ -13,3 +13,25 @@ int main(){
 
     return 0;
 }
+
+/*
+#include <stdio.h>
+
+int main(){
+    
+    int lista[3];
+    int suma = 0;
+
+    printf("Ingresar 3 numeros enteros: ");
+    scanf("%d %d %d", &lista[0], &lista[1], &lista[3]);
+
+    for (int i = 0; i < sizeof(lista) / sizeof(int); i++){
+        suma = suma + lista[i];
+    }
+    float promedio = (float) suma / sizeof(lista) * sizeof(int);
+    
+    printf("Promedio: %f", promedio);
+
+    return 0;
+}
+*/
--- Clase1/ejercicio4.c
+++ Clase1/ejercicio4.c
@@ -2,13 +2,18 @@
 
 int main(){
     
-    int lista[] = {1, 5 ,6};
-    double promedio = lista[0];
+    int lista[3];
+    int suma = 0;
 
-    for int i = 0
+    printf("Ingresar 3 numeros enteros: ");
+    scanf("%d %d %d", &lista[0], &lista[1], &lista[3]);
+
+    for (int i = 0; i < sizeof(lista) / sizeof(int); i++){
+        suma = suma + lista[i];
+    }
+    float promedio = (float) suma / sizeof(lista) * sizeof(int);
     
-    
-    
-    
+    printf("Promedio: %f", promedio);
+
     return 0;
 }
