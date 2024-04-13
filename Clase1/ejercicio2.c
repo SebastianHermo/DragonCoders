bb7e6323b05598789f688c9497516a2d158df3f8
LucasR1006 <lucasor1006@gmail.com>
2024-04-13 12:26:40 -0300

    subo ej 3.1

--- Clase1/ejercicio2.c
+++ Clase1/ejercicio2.c
@@ -5,7 +5,7 @@ int main(){
     int lista[] = {1, 25, 36, 46, 20, 0};
     int maxLista = lista[0];
    
-    for (int i = 1; i < sizeof(lista) / sizeof(int); i++){
+    for (int i = 0; i < sizeof(lista) / sizeof(int); i++){
       if (lista[i] > maxLista) {
         maxLista = lista[i];
       } 
