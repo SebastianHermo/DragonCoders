a3f91385323f9c1aeef79fa73f7df903d251902c
Sebastian Ezequiel Hermo Avila <sebastianhermoavila@gmail.com>
2024-05-18 11:56:40 -0300

    iniciando el TP

---
+++ TP/Lista.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct structNodo {
+    struct structNodo proximo;
+} Nodo;
+
+Alta(Nodo **lista) {
+
+    if esAlumno || esMateria{
+    Nodo *nuevoNodo = malloc(sizeof(Nodo));
+    nuevoNodo->valor = valor;
+    nuevoNodo->proximo = NULL;
+    if (*lista == NULL) {
+        *lista = nuevoNodo;
+    } else {
+        Nodo *cursor = *lista;
+        while (cursor->proximo != NULL) {
+            cursor = cursor->proximo;
+        }
+        cursor->proximo = nuevoNodo;
+    }
+    }
+
+   
+}
+
+Baja(){
+    if esAlumno || esMateria{
+
+    }
+
+}
+
+modificar(){
+    if esAlumno || esMateria{
+        
+    }
+
+}
+
+enlistarAlumnos(){
+
+}
+
+buscarAlumno(){
+
+}
+
+buscarAlumnoEdad(){
+
+}
+
+anotarseEnLaMateria(alumno, materia){
+
+}
+
+rendirMateria(){
+
+}
---
+++ TP/menu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <malloc.h>
+#include "Lista.c"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
