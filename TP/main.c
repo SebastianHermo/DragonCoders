55f774e5ffdd60385a17c652b79654d8cedeb184
Sebastian Ezequiel Hermo Avila <sebastianhermoavila@gmail.com>
2024-06-08 13:37:21 -0300

    comando de despedida para el menu

--- TP/main.c
+++ TP/main.c
@@ -3,110 +3,36 @@
 #include <signal.h>
 #include "menu.c"
 
+void signal_handler(int sig);
 void loadingBar();
 void menu();
 
 int main(){
     loadingBar();
-
     printf("\n\n");
-
     menu();
     return 0;
-}
-
-volatile sig_atomic_t cancel_loading = 0;
-
-
-void signal_handler(int sig) {
-    cancel_loading = 1;
-}
+}               
 
 void loadingBar() {
-    // Carácter para la barra de carga
-    char a = '#';
 
-    printf("\n\n\n\n");
-    printf("Inicializando el GestorDeALumnos, por favor aguarde un momento...\n\n");
-    printf("Presione Ctrl+C para cancelar\n");
-    printf("[");
-
-    // Tamaño de la barra de carga
-    int barLength = 30;
-
-    // Imprimir barra de carga vacía
-    for (int i = 0; i < barLength; i++) {
-        printf("-");
+    int i;
+    //    printf("\n\n\n\n");
+    printf("\n\n\t\t\tLoading...\n\n");
+    printf("\t\t\t\t\t["); // Corchete de apertura
+    for (i = 0; i < 26; i++) {
+        printf("%c", 177);
+        fflush(stdout);
+        usleep(100000); // 100ms delay
     }
-
-    printf("]");
-
-    // Volver al inicio de la barra de carga
+    printf("]"); // Corchete de cierre para la primera barra
     printf("\r");
-    printf("\t\t\t\t[");
-
-    // Instalar manejador de señal para Ctrl+C
-    signal(SIGINT, signal_handler);
-
-    // Imprimir barra de carga con progreso
-    for (int i = 0; i < barLength; i++) {
-        printf("%c", a);
-        fflush(stdout); // Asegurarse de que se imprime inmediatamente
-        usleep(100000); // Pausa de 100 ms
+    printf("\t\t\t\t\t["); // Corchete de apertura
+    for (i = 0; i < 26; i++) {
+        printf("%c", 219);
+        fflush(stdout);
+        usleep(100000); // 100ms delay
     }
-
-    printf("]\n");
-}
-
-void menu() {
-    printf("Bienvenido/s al Gestor de Alumnos por DragonCoders.\n\n"
-           "Para comenzar a utilizar el sistema, deberas cargar la informacion del primer alumno:\n\n");
-
-    char nombre [20];
-    int edad;
-    printf("Ingrese el nombre y la edad del Alumno que desea cargar (solo nombre y edad): \n");
-    scanf("%s %d", nombre, &edad);
-
-    /*AltaAlumno(nombre,edad);*/
-
-    printf("\nMuy bien hecho! A partir de esto podemos comenzar a operar.\n\n");
-
-    menuBasico();
-
-}
-
-void menuBasico(){
-
-    printf("Seleccione a continuacion la opcion que desee utilizar\n");
-    printf("1. Dar de alta un alumno\n"
-           "2. Dar de baja un alumno\n"
-           "3. Dar de alta una materia\n"
-           "4. Dar de baja una materia\n"
-           "5. Modificar datos de alumno\n"
-           "6. Modificar datos de una materia\n");
-
-    int opcion;
-    scanf("%d", &opcion);
-
-    switch (opcion) {
-        case 1:
-            printf("\nFausto no hizo el codigo todavia");
-            break;
-        case 2:
-            printf("\nFausto no hizo el codigo todavia");
-            break;
-        case 3:
-            printf("\nFausto no hizo el codigo todavia");
-            break;
-        case 4:
-            printf("\nFausto no hizo el codigo todavia");
-            break;
-        case 5:
-            printf("\nFausto no hizo el codigo todavia");
-            break;
-        case 6:
-            printf("\nFausto no hizo el código todavía");
-            break;
-    }
+    printf("]"); // Corchete de cierre para la segunda barra
 
 }
--- TP/menu.c
+++ TP/menu.c
@@ -1,38 +1,64 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <ctime>
 
 void menu(){
 
-    printf("Seleccione a continuacion la opcion que desee utilizar\n");
+    printf("Seleccione a continuación la opción que desee utilizar\n");
     printf("1. Dar de alta un alumno\n"
            "2. Dar de baja un alumno\n"
            "3. Dar de alta una materia\n"
            "4. Dar de baja una materia\n"
            "5. Modificar datos de alumno\n"
-           "6. Modificar datos de una materia\n");
+           "6. Modificar datos de una materia\n"
+           "7. Salir\n");
 
     int opcion;
     scanf("%d", &opcion);
 
     switch (opcion) {
         case 1:
-            printf("\nFausto no hizo el codigo todavia");
+            printf("\nFausto no hizo el código todavía");
             break;
         case 2:
-            printf("\nFausto no hizo el codigo todavia");
+            printf("\nFausto no hizo el código todavía");
             break;
         case 3:
-            printf("\nFausto no hizo el codigo todavia");
+            printf("\nFausto no hizo el código todavía");
             break;
         case 4:
-            printf("\nFausto no hizo el codigo todavia");
+            printf("\nFausto no hizo el código todavía");
             break;
         case 5:
-            printf("\nFausto no hizo el codigo todavia");
+            printf("\nFausto no hizo el código todavía");
             break;
         case 6:
-            printf("\nFausto no hizo el codigo todavia");
+            printf("\nFausto no hizo el código todavía");
             break;
+        case 7:
+            despedida();
     }
+}
 
+    void despedida(){
+    time_t now = time(NULL);        
+    struct tm *tm = localtime(&now);
+    int hour = tm->tm_hour;
+    char *greeting;
+    if (hour < 12) {
+        greeting = "Buenos días";
+    } else if (hour < 18) {
+        greeting = "Buenas tardes";
+    } else {
+        greeting = "Buenas noches";
+    }
+    printf("%s, gracias por utilizar nuestra aplicación.\n", greeting);
+    printf("Esperamos que hayas disfrutado de nuestra aplicación. ¡Hasta luego!\n");
+
+    // Cerrar la consola
+    #ifdef _WIN32
+        system("exit"); // Para Windows
+    #else
+        system("clear"); // Para Linux y macOS
+    #endif
 }
