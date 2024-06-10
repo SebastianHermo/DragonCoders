308933f93482aaccd8eb2a718ffc3ecb019672f7
Fausto Garcia <garciafausto0904@gmail.com>
2024-06-10 16:54:55 -0300

    no se

--- TP/main.c
+++ TP/main.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "funciones.c"
+#include <signal.h>
+#include "struct.h"
+#include "menu.c"
 
 void loadingBar();
-void menu();
-void menuBasico();
-void menuBasico2(int);
-void menuGestionAlumnos();
-void menuGestionMaterias();
 
-Alumno *listaAlumnos = NULL;
-Materia *listaMaterias = NULL;
-
-int main() {
-    //loadingBar();
-
+int main(){
+    Alumno *listaAlumno = NULL;
+    Materia *listaMateria = NULL;
+    loadingBar();
     printf("\n\n");
-
-    menu();
+    menu(listaAlumno, listaMateria);
     return 0;
 }
 
-void loadingBar() {
+void loadingBar(){
 
     int i;
     //    printf("\n\n\n\n");
     printf("\n\n\t\t\tLoading...\n\n");
     printf("\t\t\t\t\t["); // Corchete de apertura
-    for (i = 0; i < 26; i++) {
+    for (i = 0; i < 26; i++){
         printf("%c", 177);
         fflush(stdout);
         usleep(100000); // 100ms delay
@@ -35,215 +29,10 @@ void loadingBar() {
     printf("]"); // Corchete de cierre para la primera barra
     printf("\r");
     printf("\t\t\t\t\t["); // Corchete de apertura
-    for (i = 0; i < 26; i++) {
+    for (i = 0; i < 26; i++){
         printf("%c", 219);
         fflush(stdout);
         usleep(100000); // 100ms delay
     }
     printf("]"); // Corchete de cierre para la segunda barra
-
-}
-
-
-void menu() {
-    printf("Bienvenido/s al Gestor de Alumnos por DragonCoders.\n\n"
-           "Para comenzar a utilizar el sistema, deberás cargar la información del primer alumno:\n\n");
-
-   
-    char nombre [20];
-    int edad;
-    printf("Ingrese el nombre de pila del Alumno que desea cargar: \n");
-    scanf("%s", nombre);
-    printf("Perfecto, a continuación ingrese la edad de dicho alumno : \n");
-    scanf("%d", &edad);
-
-    altaAlumno(&listaAlumnos, nombre, edad);
-    printf("%s\t",listaAlumnos->nombre);
-    printf("%d\n",listaAlumnos->edad);
-
-    printf("\nMuy bien hecho! A partir de esto podemos comenzar a operar, a partir de distintas funciones para los alumnos y sus respectivas materias cursadas o en curso.\n\n");
-
-    menuBasico();
-
-}
-
-void menuBasico(){
-
-    int opcion;
-
-    printf("1. Alumnos\n");
-    printf("2. Materias\n");
-    printf("3. Cerrar el sistema\n");
-
-    scanf("%d", &opcion);
-    menuBasico2(opcion);
-}
-
-void menuBasico2(int opcion) {
-
-    if (opcion < 1 || opcion > 3) {
-        printf("La opción ingresada no es valida, porfavor vuelva a ingresar la opción que precise.");
-        menuBasico();
-    } else {
-        switch (opcion) {
-            case 1:
-                menuGestionAlumnos();
-                break;
-            case 2:
-                menuGestionMaterias();
-                break;
-            case 3:
-               break;
-        }
-    }
-}
-
-void menuGestionAlumnos(){
-
-    printf("Seleccione a continuación la opción que desee utilizar\n");
-
-    printf("1. Dar de alta un alumno\n"
-           "2. Dar de baja un alumno \n"
-           "3. Modificar datos de un alumno\n"
-           "4. Modificar regularidad en materia de un alumno\n"
-           "5. Volver al menú principal\n");
-
-
-    int opcion;
-    scanf("%d", &opcion);
-
-    if (opcion < 1 || opcion > 4){
-        printf("La opción ingresada no es valida, porfavor vuelva a ingresar la opción que precise.");
-        menuGestionAlumnos();
-    } else {
-        char alumno[20];
-        int edad;
-        char nuevoNombre[20];
-        int nuevaEdad;
-        char materia[20];
-        int regularidad;
-
-        int contador = 1;
-
-        switch (opcion) {
-            case 1:
-
-                printf("Ingrese el nombre del alumno y su edad\n");
-                scanf("%s %d", alumno, &edad);
-
-                altaAlumno(&listaAlumnos, alumno, edad);
-
-                if (contador==1){
-
-                printf("%s\t", listaAlumnos->proximo->nombre);
-                printf("%d\n", listaAlumnos->proximo->edad);
-
-                }else{
-                
-                printf("%s\t", listaAlumnos->proximo->proximo->nombre);
-                printf("%d\n", listaAlumnos->proximo->proximo->edad);
-                }
-                contador = contador+1;
-
-                printf("El alumno fue ingresado con exito\n");
-
-                menuBasico();
-                
-                break;
-
-                case 2:
-                    
-                    printf("Ingrese el nombre del alumno\n");
-                    scanf("%s", alumno);
-
-                    bajaAlumno(&listaAlumnos, alumno);
-
-                    printf("El alumno fue eliminado con exito\n");
-
-                    menuBasico();
-                    break;
-
-                    case 3:
-
-                        printf("Ingrese el nombre actual del alumno, su nuevo nombre y su nueva edad\n");
-                        scanf("%s %s %d", alumno, nuevoNombre, &nuevaEdad);
-
-                        modificarAlumno(&listaAlumnos, alumno, nuevoNombre, nuevaEdad);
-
-                        printf("El nombre fue cambiado con exito\n");
-
-                        menuBasico();
-                        break;
-
-                        case 4:
-
-                            printf("Ingrese el nombre del alumno, una materia y su regularidad(1=regular / 0=no regular)\n");
-                            scanf("%s %s %d", alumno, materia, &regularidad);
-
-                            modificarMateriaAlumno(&listaAlumnos, alumno, materia, regularidad);
-
-                            printf("La regularidad se modifico con exito\n");
-                            menuBasico();
-                            break;
-
-                            case 5:
-                            menuBasico();
-                            break;
-        }
-    }
-}
-
-void menuGestionMaterias(){
-
-    printf("Seleccione a continuación la opción que desee utilizar\n");
-
-    printf("1. Dar de alta una materia\n"
-           "2. Dar de baja una materia \n"
-           "3. Modificar datos de una materia\n"
-           "4. Volver al menú principal\n");
-
-    int opcion;
-    scanf("%d", &opcion);
-
-    if (opcion < 1 || opcion > 4){
-        printf("La opción ingresada no es valida, porfavor vuelva a ingresar la opción que precise.");
-        menuGestionMaterias();
-
-    } else {
-        char materia[20];
-
-        switch (opcion) {
-            case 1:
-
-                printf("Ingrese el nombre de la materia\n");
-                scanf("%s", materia);
-
-                altaMateria(&listaMaterias,materia);
-
-                printf("La materia fue ingresada con exito\n");
-
-                menuBasico();
-                
-                break;
-            case 2:
-
-                printf("Ingrese el nombre de la materia\n");
-                scanf("%s", materia);
-
-                bajaMateria(&listaMaterias,materia);
-
-                printf("La materia fue eliminada con exito\n");
-
-                menuBasico();
-                
-                break;
-            case 3:
-                printf("\nFausto no hizo el código todavía.\n");
-                break;
-            case 4:
-
-                break;
-
-        }
-    }
 }
--- TP/struct.h
+++ TP/struct.h
@@ -2,19 +2,24 @@
 #include <stdbool.h>
 #include <string.h>
 
+#ifndef STRUCT_H
+#define STRUCT_H
+
 // Estructura de nodo para el alumno
-typedef struct Alumno {
-    char* nombre;
+typedef struct primerNodo{
+    char *nombre;
     int edad;
-    struct Materia* materias; // lista de materias individuales
-    struct Alumno* proximo;
+    struct Materia *materias; // lista de materias individuales
+    struct Alumno *proximo;
 } Alumno;
 
 // Estructura de nodo para la materia
-typedef struct Materia {
-    char* nombre;
+typedef struct segundoNodo{
+    char *nombre;
     int estado; // 0 = no aprobado, 1 = aprobado
     float nota;
     int regularidad; // 0 = irregular, 1 = regular
-    struct Materia* proximo;
+    struct Materia *proximo;
 } Materia;
+
+#endif
